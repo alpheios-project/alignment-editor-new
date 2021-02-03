246b36b0b512e42a43e1a86d03f01f67b5e425f0
Irina <irina060981@yandex.ru>
2021-02-03 17:44:29 +1000

    separate components for output

--- src/_output/index.js
+++ src/_output/index.js
@@ -1,6 +1,8 @@
 import OutputStyle from '@/_output/styles/style.scss'
 import App from '@/_output/vue/app.vue'
 
+import Vue from '@vue-runtime'
+
 export {
-  OutputStyle, App
+  OutputStyle, App, Vue
 }
