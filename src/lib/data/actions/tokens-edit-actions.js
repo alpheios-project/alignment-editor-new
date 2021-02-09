4550aad3bff175244f1ccae34575aece22f8079d
Irina <irina060981@yandex.ru>
2021-02-09 10:16:19 +1000

    allowedDelete is false for the last word in a segment

--- src/lib/data/actions/tokens-edit-actions.js
+++ src/lib/data/actions/tokens-edit-actions.js
@@ -314,8 +314,10 @@ export default class TokensEditActions {
    */
   allowedDelete (token) {
     const alignedText = this.getAlignedTextByToken(token)
-    return (!this.getNextPrevToken(token, HistoryStep.directions.PREV) && (token.segmentIndex === alignedText.segments[0].index)) ||
-           (!this.getNextPrevToken(token, HistoryStep.directions.NEXT) && (token.segmentIndex === alignedText.segments[alignedText.segments.length - 1].index))
+    const segment = this.getSegmentByToken(token)
+    return segment.tokens.length > 1 &&
+           ((!this.getNextPrevToken(token, HistoryStep.directions.PREV) && (token.segmentIndex === alignedText.segments[0].index)) ||
+           (!this.getNextPrevToken(token, HistoryStep.directions.NEXT) && (token.segmentIndex === alignedText.segments[alignedText.segments.length - 1].index)))
   }
 
   /**
