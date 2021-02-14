a1dff4d37f0919f8982216e3f91813dfe0e29194
Irina <irina060981@yandex.ru>
2021-02-14 16:05:31 +1000

    update options for alpheios reading tools

--- public/dist/alpheios-alignment-editor.js
+++ public/dist/alpheios-alignment-editor.js
@@ -46131,7 +46131,7 @@ __webpack_require__.r(__webpack_exports__);
 class StoreDefinition {
   // A build name info will be injected by webpack into the BUILD_NAME but need to have a fallback in case it fails
   static get libBuildName () {
-    return  true ? "i68-enable-alpheios-tools.20210214476" : 0
+    return  true ? "i68-enable-alpheios-tools.20210214482" : 0
   }
 
   static get libName () {
