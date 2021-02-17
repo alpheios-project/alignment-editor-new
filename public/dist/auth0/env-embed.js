d6b68d10c2d3dba9429d26ffe83dc18aa630c7df
Irina <irina060981@yandex.ru>
2021-02-17 21:22:22 +1000

    add auth0 settings

---
+++ public/dist/auth0/env-embed.js
@@ -0,0 +1,10 @@
+const auth0Env = {
+  DOMAIN: 'alpheios.auth0.com',
+  CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
+  ENDPOINTS: {
+    wordlist: 'https://userapis.alpheios.net/v1/words',
+    settings: 'https://settings.alpheios.net/v1/settings'
+  },
+  AUDIENCE: 'alpheios.net:apis',
+  LOGOUT_URL: 'https://alignment.alpheios.net/index.html'
+}
