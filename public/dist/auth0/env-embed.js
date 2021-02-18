e5c7efadab8f321cdf573beb452fc180adf4682e
Irina <irina060981@yandex.ru>
2021-02-18 15:15:28 +1000

    update workflow

--- public/dist/auth0/env-embed.js
+++ public/dist/auth0/env-embed.js
@@ -1,6 +1,6 @@
 const auth0Env = {
   DOMAIN: 'alpheios.auth0.com',
-  CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
+  CLIENT_ID: 'PLACE_AUTH0_CLIENT_ID_HERE',
   ENDPOINTS: {
     wordlist: 'https://userapis.alpheios.net/v1/words',
     settings: 'https://settings.alpheios.net/v1/settings'
