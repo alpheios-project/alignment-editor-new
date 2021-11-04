abd576b0354ff668910fd8c5ff52a83f6817dd3a
Irina <irina060981@yandex.ru>
2021-11-04 10:42:29 +1000

    fix bug with clear tokens edit history after grouping inserted tokens

--- public/dist/alpheios-alignment-editor.js
+++ public/dist/alpheios-alignment-editor.js
@@ -45277,7 +45277,13 @@ class TokensEditHistory extends _lib_data_history_editor_history__WEBPACK_IMPORT
   }
 
   tokenWasEdited (token) {
-    return this.steps.some(step => step.token === token)
+    return this.steps.some(step => {
+      if (step.type === _lib_data_history_history_step_js__WEBPACK_IMPORTED_MODULE_2__["default"].types.NEW) {
+        return step.params.createdTokens.some(createdToken => createdToken.idWord === token.idWord)
+      } else {
+        return step.token.idWord === token.idWord
+      }
+    })
   }
 
   updateLastStepWithAnnotations (annotations, idWord) {
@@ -48270,7 +48276,7 @@ __webpack_require__.r(__webpack_exports__);
 class StoreDefinition {
   // A build name info will be injected by webpack into the BUILD_NAME but need to have a fallback in case it fails
   static get libBuildName () {
-    return  true ? "i576-insert-tokens-2.20211103614" : 0
+    return  true ? "i592-clear-history-bug.20211104320" : 0
   }
 
   static get libName () {
