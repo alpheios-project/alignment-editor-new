3db06d47c7fc25cc14aa32e6911c84c3ec965702
Irina <irina060981@yandex.ru>
2022-03-18 13:13:52 +1000

    small fix

--- public/dist/alpheios-alignment-editor.js
+++ public/dist/alpheios-alignment-editor.js
@@ -48999,7 +48999,7 @@ __webpack_require__.r(__webpack_exports__);
 class StoreDefinition {
   // A build name info will be injected by webpack into the BUILD_NAME but need to have a fallback in case it fails
   static get libBuildName () {
-    return  true ? "i706-autoclose-errors.20220318394" : 0
+    return  true ? "i706-autoclose-errors.20220318397" : 0
   }
 
   static get libName () {
