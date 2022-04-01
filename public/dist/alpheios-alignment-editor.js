7b63577057d01fa9397044c9282a05f3c9f188e9
Irina <irina060981@yandex.ru>
2022-04-01 11:48:17 +1000

    html layout changes

--- public/dist/alpheios-alignment-editor.js
+++ public/dist/alpheios-alignment-editor.js
@@ -48999,7 +48999,7 @@ __webpack_require__.r(__webpack_exports__);
 class StoreDefinition {
   // A build name info will be injected by webpack into the BUILD_NAME but need to have a fallback in case it fails
   static get libBuildName () {
-    return  true ? "i708-html-cosmetic.20220325387" : 0
+    return  true ? "i710-cosmetic-html.20220401354" : 0
   }
 
   static get libName () {
