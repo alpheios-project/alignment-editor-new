e6b1566594cbc821a7d39fd43ee7509dd5acb4c9
Irina <irina060981@yandex.ru>
2022-04-01 13:02:33 +1000

    update tooltips, notices and help screen on Token Editor screen

--- src/vue/help-blocks/eng/help-block-edit.vue
+++ src/vue/help-blocks/eng/help-block-edit.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="alpheios-editor-help-content">
-    <p>After texts have been prepared for alignment, which involves their analysis into "tokens", 
-      they cannot be edited directly, but can only be modified with the special "Token Editor", 
-      which has its own screen. It is therefore advisable to review the texts carefully 
-      before preparing them for alignment by tokenization.
+    <p>After texts have been prepared for alignment, which requires their analysis into "tokens", 
+      they cannot be edited directly, but can only be modified with this special Token Editor. 
+      It is therefore advisable to review your texts carefully before pressing [DONE], which tokenizes them.
     </p>
-    <p>Furthermore, any words that have already been aligned must be unlinked in the alignment screen 
-      before their tokens can be edited in any way in the edit screen. If you try to edit them 
+    <p>Furthermore, any words that have already been aligned must be unlinked in the Align Text screen before 
+      their tokens can be edited in this screen. If you try to edit tokens that are still aligned, 
       you will receive a message prompting you to unlink them first. 
     </p>
-    <p>What you can still do: (by clicking on a word to bring up the small options icons)</p>
+    <p>Clicking on a "token" on this screen will open a panel of editing icons
+    (which you can reduce in number in the Options if you find you do not need some).
+    </p>
+    <p>By choosing the appropriate small icon (identifiable from their tool tips) you can then
+    </p>
     <ul>
-      <li>you can add or delete line breaks (by choosing the appropriate small icon)</li>
-      <li>you can correct the spelling of a word (by making the change and then clicking on the pencil icon to save the change)</li>
-      <li>you can merge two adjacent words and subsequently split them again (by choosing the appropriate small icon)</li>
-    </ul>
-    <p>What you cannot do after "tokenization" or during alignment:</p>
-    <ul>
-      <li>you cannot add or delete a word except at the beginning or end of whatever text you tokenized 
-        If you need to do so you will have to re-enter your text on the Text Enter Screen and retokenize with the 
-        "Prepare Texts for Alignment" button.
-      </li>
+      <li>add or delete line breaks</li>
+      <li>add or delete tokens (words or punctuation)</li>
+      <li>merge adjacent tokens and subsequently split them again</li>
+      <li>edit a token (e.g. correct the spelling of a word)
+    (to save such editing changes you MUST click on the pencil icon to save it after making the change)</li>
     </ul>
     <p>All successful changes will be immediately visible in the Alignment Screen</p>
   </div>
