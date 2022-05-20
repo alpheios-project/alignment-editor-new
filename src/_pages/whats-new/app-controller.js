2df66e05078a2fa653c9f0d1c34367831e8a62f2
Irina <irina060981@yandex.ru>
2022-05-20 12:33:29 +1000

    rebuild

--- src/_pages/whats-new/app-controller.js
+++ src/_pages/whats-new/app-controller.js
@@ -3,10 +3,10 @@ import Splide from '@splidejs/splide'
 export default class AppController {
   constructor ({ splideId }) {
     console.info('splideId - ', splideId)
-    new Splide( `#${splideId}`, {
+    new Splide(`#${splideId}`, {
       autoplay: true,
       interval: 3000,
       pauseOnHover: true
-    } ).mount()
+    }).mount()
   }
 }
--- src/_pages/whats-new/index.js
+++ src/_pages/whats-new/index.js
@@ -1,4 +1,4 @@
-import '@splidejs/splide/css';
+import '@splidejs/splide/css'
 import OutputStyle from '@/_pages/whats-new/styles/style.scss'
 import AppController from '@/_pages/whats-new/app-controller.js'
 
