1cd802b3236139b0021a2a9a174bbf2bbe0b9d6c
Irina <irina060981@yandex.ru>
2022-05-20 11:22:34 +1000

    remove vue from youtube tutorials page

--- src/_pages/video-tutorials/index.js
+++ src/_pages/video-tutorials/index.js
@@ -1,9 +1,6 @@
 import OutputStyle from '@/_pages/video-tutorials/styles/style.scss'
 import AppController from '@/_pages/video-tutorials/app-controller.js'
-import App from '@/_pages/video-tutorials/vue/app.vue'
-
-import Vue from '@vue-runtime'
 
 export {
-  OutputStyle, Vue, App, AppController
+  OutputStyle, AppController
 }
