cfce3dc9e03910de59561c7d98ee60affb01c9bd
Irina <irina060981@yandex.ru>
2022-07-12 15:36:40 +1000

    reorganize chunks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+// import Fonts from '@/styles/_fonts.scss'
 import Style from '@/styles/style.scss'
 import AppController from '@/lib/controllers/app-controller.js'
+
+import(/* webpackPreload: true */ '@/styles/_fonts.scss')
 
 export {
   Style, AppController
